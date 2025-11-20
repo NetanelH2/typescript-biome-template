876c149ef17c5efc0872bb40e6ee1bb4258b8655
Netanel Harush <netanelharush2@gmail.com>
2025-11-20 14:40:33 +0200

    Update K6 test script to use correct URL and add iterations option

--- src/tests/script.ts
+++ src/tests/script.ts
@@ -3,11 +3,12 @@ import http from 'k6/http'
 
 export const options = {
 	vus: 10,
+	iterations: 300,
 	duration: '30s',
 }
 
 export default function () {
-	const res = http.get('https://quickpizza.grafana.com')
+	const res = http.get('https://www.saucedemo.com')
 	check(res, {
 		'is status 200': (r) => r.status === 200,
 	})
